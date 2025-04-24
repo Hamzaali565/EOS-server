98643d99c665681de4e9e4c4cbc78e0c018f34c1
Hamzaali565 <ha6153806@gmail.com>
2025-04-24 21:43:03 +0500

    new changes in cors

--- src/app.mjs
+++ src/app.mjs
@@ -12,6 +12,8 @@ app.use(
       "http://localhost:3000",
       "https://www.eos-ltd.com",
       "http://www.eos-ltd.com",
+      "https://eos-ltd.com",
+      "http://eos-ltd.com",
       "https://landing-page-for-practice.vercel.app",
     ],
   })
