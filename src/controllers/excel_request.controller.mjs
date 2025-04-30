c1adcbda927c3dd739c08b73c77b077206568ed6
Hamzaali565 <ha6153806@gmail.com>
2025-04-30 22:43:13 +0500

    Add validations

--- src/controllers/excel_request.controller.mjs
+++ src/controllers/excel_request.controller.mjs
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { sendMail } from "../helper/mail.helper.mjs";
 import { excel_request_model } from "../models/excel_request.model.mjs";
 import { emailMessage } from "../public/email.request.mjs";
@@ -8,6 +9,17 @@ const excel_request = async (req, res) => {
 
     if (![username, email, product_detail].every(Boolean)) {
       return res.status(404).json({ message: "All Parameters are required" });
+    }
+
+    const attemps_check = await excel_request_model
+      .findOne({ email })
+      .sort({ createdAt: -1 });
+    if (attemps_check) {
+      const isOlderThanOneDay =
+        moment().diff(moment(attemps_check?.createdAt), "days") >= 1;
+      if (!isOlderThanOneDay) {
+        return res.status(401).json({ message: "Request already generated." });
+      }
     }
     const response = new excel_request_model({
       email,
